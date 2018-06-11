86dd664e8ed2bcabaef3ae58156499213f806e42
Michal Klement <miklement@icloud.com>
2018-06-11 22:09:07 +0200

    v2

--- problems/007_Encoding.swift
+++
@@ -1,9 +0,0 @@
-// This problem was asked by Facebook.
-//
-// Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
-//
-// For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
-//
-// author: Michal Klement, miklement@icloud.com
-//
-
